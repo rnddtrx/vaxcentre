30dfc1cf42e9c693a88ca906663a3bd8a07c4cd3
rnddtx <renaud.dutrieux@hainaut-promsoc.be>
2023-03-14 20:43:05 +0100

    update

---
+++ target/classes/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO Centres (Name) VALUES ('Center 1');
+INSERT INTO Centres (Name) VALUES ('Center 2');
+INSERT INTO Centres (Name) VALUES ('Center 3');
+INSERT INTO Centres (Name) VALUES ('Center 4');
+INSERT INTO Centres (Name) VALUES ('Center 5');
+
+INSERT INTO Persons (Lastname, Firstname) VALUES ('Smith', 'John');
+INSERT INTO Persons (Lastname, Firstname) VALUES ('Johnson', 'Jane');
+INSERT INTO Persons (Lastname, Firstname) VALUES ('Williams', 'James');
+INSERT INTO Persons (Lastname, Firstname) VALUES ('Brown', 'Ryan');
+
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-05-01', 1, 1);
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-06-02', 2, 1);
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-04-03', 3, 2);
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-05-04', 4, 2);
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-04-30', 5, 3);
+INSERT INTO Schedules (ScheduleDate, CentreId, PersonId) VALUES ('2022-05-10', 1, 4);
